f1723a6703a76e57b8ca9613f64a99a5604fd1ac
AbhishekLayek <abhisheklayek57@gmail.com>
2026-05-03 22:50:57 +0530

    Adapter Design Pattern

---
+++ adapter_design_pattern/AndroidCharger.java
@@ -0,0 +1,5 @@
+package adapter_design_pattern;
+
+public interface AndroidCharger {
+	void chargerAndroidPhone();
+}
---
+++ adapter_design_pattern/OnePlusCharger.java
@@ -0,0 +1,10 @@
+package adapter_design_pattern;
+
+public class OnePlusCharger implements AndroidCharger{
+
+	@Override
+	public void chargerAndroidPhone() {
+		System.out.println("Your Android Phone Is Charging...");
+	}
+
+}
